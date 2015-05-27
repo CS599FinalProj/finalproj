be403825821c8aef0e0077b727a6d5f63129878d
jeffwang1987 <jeffwang1987@hotmail.com>
2015-05-26 21:36:38 -0700

    insert

--- src/main/java/gui.java
+++ src/main/java/gui.java
@@ -1,7 +1,10 @@
+import org.jfree.ui.RefineryUtilities;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
@@ -10,28 +13,45 @@ public class gui extends JFrame
     private JButton button1;
     private JButton button2;
     private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
 
-    private String path;
+    private String path = "";
+    private String name = "";
+
+    private ArrayList<Double> positiveComment = new ArrayList<Double>();
+    private ArrayList<Double> negativeComment = new ArrayList<Double>();
+    private ArrayList<String> fileName = new ArrayList<String>();
 
     public gui()
     {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(3, 1));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new GridLayout(6, 1));
 
 
         button1 = new JButton("Choosing Testing Data");
         button2 = new JButton("Naive Bayes");
         button3 = new JButton("SVM");
+        button4 = new JButton("Evaluation");
+        button5 = new JButton("Clear");
+        button6 = new JButton("Exit");
 
         button1.addActionListener(new button1());
         button2.addActionListener(new button2());
         button3.addActionListener(new button3());
+        button4.addActionListener(new button4());
+        button5.addActionListener(new button5());
+        button6.addActionListener(new button6());
 
         add(button1);
         add(button2);
         add(button3);
+        add(button4);
+        add(button5);
+        add(button6);
 
-        setSize(400, 400);
+        setSize(300, 500);
         setVisible(true);
 
     }
@@ -46,6 +66,8 @@ public class gui extends JFrame
             {
                 File selectedFile = fileChooser.getSelectedFile();
                 path = selectedFile.getPath();
+                name = fileChooser.getSelectedFile().getName();
+                name = name.substring(0, name.lastIndexOf("."));
             }
         }
     }
@@ -54,13 +76,23 @@ public class gui extends JFrame
     {
         public void actionPerformed(ActionEvent e)
         {
-            try {
-                nbGenerator nb = new nbGenerator(path);
-                int total = nb.numberOfPositive + nb.getNumberOfNegative;
-                System.out.println("Total number is " + total);
-                System.out.println("Positive proportion is " + nb.positiveProportion);
-            } catch (IOException e1) {
-                e1.printStackTrace();
+            if(path.equals(""))
+            {
+                JOptionPane.showMessageDialog(null, "Please choose a testing data");
+            }
+            else {
+                try {
+                    nbGenerator nb = new nbGenerator(path);
+                    int total = nb.numberOfPositive + nb.getNumberOfNegative;
+                    System.out.println("Total number is " + total);
+                    System.out.println("Positive proportion is " + nb.positiveProportion);
+                    positiveComment.add(nb.numberOfPositive*1.0/total);
+                    negativeComment.add(nb.getNumberOfNegative*1.0/total);
+                    fileName.add(name);
+                    generateChart("Naive Bayes");
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
     }
@@ -69,16 +101,63 @@ public class gui extends JFrame
     {
         public void actionPerformed(ActionEvent e)
         {
-            svmClassifer svmc = new svmClassifer(path);
-            try {
-                svmc.run();
-                int total = svmc.numberOfNegative + svmc.numberOfPositive;
-                System.out.println("Total number is " + total);
-                System.out.println("Positive proportion is " + svmc.positiveProportion);
-            } catch (IOException e1) {
-                e1.printStackTrace();
+            if(path.equals(""))
+            {
+                JOptionPane.showMessageDialog(null, "Please choose a testing data");
+            }
+            else
+            {
+                svmClassifer svmc = new svmClassifer(path);
+                try {
+                    svmc.run();
+                    int total = svmc.numberOfNegative + svmc.numberOfPositive;
+                    System.out.println("Total number is " + total);
+                    System.out.println("Positive proportion is " + svmc.positiveProportion);
+                    positiveComment.add(svmc.numberOfPositive*1.0/total);
+                    negativeComment.add(svmc.numberOfNegative*1.0/total);
+                    fileName.add(name);
+                    generateChart("SVM");
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
+    }
+
+    private class button4 implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+
+        }
+    }
+
+    private class button5 implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            positiveComment.clear();
+            negativeComment.clear();
+            fileName.clear();
+            path = "";
+            name = "";
+        }
+    }
+
+    private class button6 implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            System.exit(0);
+        }
+    }
+
+    public void generateChart(String str)
+    {
+        Chart chart = new Chart(str, positiveComment, negativeComment, fileName);
+        chart.pack(); // 動態設定元件大小
+        RefineryUtilities.centerFrameOnScreen(chart);
+        chart.setVisible(true);
     }
 
     public static void main(String[] args)
